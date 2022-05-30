06e28d09be12a92d2b860cce624247cd19388438
Agustin <agussblanco@icloud.com>
2022-05-29 23:06:41 -0500

    Header

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,18 @@
+var d = document;
+var w = window;
+
 var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
 var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
   return new bootstrap.Popover(popoverTriggerEl)
 })
 
+
+d.addEventListener("scroll", (e) => {
+  var header = d.querySelector("header");
+  var navbar = d.querySelector(".nav-bkg");
+  if(w.scrollY >= (header.offsetTop + header.offsetHeight) - 150){
+    navbar.classList.add("nav-sticky")
+  } else {
+    navbar.classList.remove("nav-sticky")
+  }
+})
